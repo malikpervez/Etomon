fe9c075f660f46bbd6200860e4ad51e5e0079ab4
dutchkillsg <mip12793@gmail.com>
2018-01-22 10:48:07 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+      $('.your-class').slick({
+         infinite: true,
+  		 slidesToShow: 3,
+  	     slidesToScroll: 3,
+  	     
+      });
+    });
+
+$('#myCarousel').carousel({
+  interval: 40000
+});
+
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+ 
+      next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');
+      
+  }
+  else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+     
+  }
+});
