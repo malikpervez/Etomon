754998bc80ee994013f21368dd8e7b0e080041a3
dutchkillsg <mip12793@gmail.com>
2018-01-23 16:36:58 -0500

    added most popular courses

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 //calls slick function for image slider
 $(document).ready(function(){
-      $('.your-class').slick({
+      $('.slick-slider').slick({
          infinite: true,
   		   slidesToShow: 3,
-  	     slidesToScroll: 3,
+  	     slidesToScroll: 1,
          prevArrow:"<img class='a-left control-c prev slick-prev' src='./images/left-arrow.png'>",
          nextArrow:"<img class='a-right control-c next slick-next' src='./images/right-arrow.png'>"
   	     
